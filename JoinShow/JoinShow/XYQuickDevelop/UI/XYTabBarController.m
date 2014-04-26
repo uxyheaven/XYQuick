5daa9cd2be9a4830f6140b4df046ec13454c14d5
Heaven <uxyheaven@163.com>
2014-04-26 14:43:34 +0800

    1

---
+++ JoinShow/JoinShow/XYQuickDevelop/UI/XYTabBarController.m
@@ -0,0 +1,235 @@
+//
+//  XYBaseTabBarController.m
+//  JoinShow
+//
+//  Created by Heaven on 14-4-25.
+//  Copyright (c) 2014年 Heaven. All rights reserved.
+//
+
+#import "XYTabBarController.h"
+
+@interface XYTabBarController ()
+
+@property (nonatomic, retain) NSMutableArray *viewControllers;
+@property (nonatomic, retain) NSArray *tempItems;
+@property (nonatomic, retain) UIView *contentView;
+
+@end
+
+@implementation XYTabBarController
+
+-(id) initWithViewControllers:(NSArray *)vcs items:(NSArray *)items{
+    self = [super init];
+    if (self) {
+        self.viewControllers = vcs;
+        self.tempItems = items;
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+-(void) createFields {
+    [super createFields];
+    
+    _tabBarFrame = CGRectMake(0, self.view.bounds.size.height - 49, self.view.bounds.size.width, 49);
+    _contentFrame = CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height - 49);
+}
+
+-(void) destroyFields {
+    [super destroyFields];
+    
+    self.viewControllers = nil;
+}
+
+-(void) createViews {
+    [super createViews];
+    
+    _contentView = [[[UIView alloc] initWithFrame:_contentFrame] autorelease];
+    [self.view addSubview:_contentView];
+    
+    _tabBar = [[[XYTabBar alloc] initWithFrame:_tabBarFrame items:_tempItems] autorelease];
+    [self.view addSubview:_tabBar];
+
+    self.tempItems = nil;
+    
+    for (int i = 0; i < _tabBar.items.count; i++) {
+        [self setupItem:_tabBar.items[i] index:i];
+    }
+    
+    UIImageView *view = [[[UIImageView alloc] initWithFrame:CGRectZero] autorelease];
+    view.alpha = 1;
+    view.backgroundColor = [UIColor yellowColor];
+    _tabBar.animatedView = view;
+    [_tabBar addSubview:view];
+}
+
+-(void) destroyViews {
+    [super destroyViews];
+}
+
+-(void) createEvents {
+    [super createEvents];
+    
+    _tabBar.delegate = self;
+}
+
+-(void) destroyEvents {
+    [super destroyEvents];
+    
+    _tabBar.delegate = nil;
+}
+
+-(void) loadData {
+    [super loadData];
+    
+    self.selectedIndex = 0;
+}
+
+
+-(UIViewController *) selectedViewController
+{
+    return [_viewControllers objectAtIndex:_selectedIndex];
+}
+
+-(void)setSelectedIndex:(NSUInteger)index
+{
+    [self displayViewAtIndex:index];
+    [_tabBar selectTabAtIndex:index];
+}
+-(void) setupItem:(UIButton *)item index:(NSInteger)index{
+    [_tabBar setupItem:item index:index];
+}
+
+-(void) resetAnimatedView:(UIImageView *)animatedView index:(NSInteger)index {
+    static BOOL isFirst = NO;
+    if (!isFirst) {
+        animatedView.backgroundColor = [UIColor orangeColor];
+        animatedView.alpha = 0.5;
+        isFirst = YES;
+    }
+    
+    [_tabBar resetAnimatedView:animatedView index:index];
+}
+
+#pragma mark - rewrite
+// 额外的重写的父类的方法
+-(void) viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+}
+
+-(void) viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+}
+
+-(void) viewWillDisappear:(BOOL)animated{
+    [super viewWillDisappear:animated];
+}
+
+-(void) viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+}
+
+-(BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+-(void) didReceiveMemoryWarning{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - private
+// 私有方法
+
+-(void) displayViewAtIndex:(NSUInteger)index{
+    UIViewController *targetViewController = [self.viewControllers objectAtIndex:index];
+    // If target index is equal to current index.
+    if (_selectedIndex == index && [[_contentView subviews] count] != 0)
+    {
+        if ([targetViewController isKindOfClass:[UINavigationController class]]) {
+            [(UINavigationController*)targetViewController popToRootViewControllerAnimated:YES];
+        }
+        return;
+    }
+    
+    _selectedIndex = index;
+    
+    [_contentView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview) withObject:nil];
+	targetViewController.view.frame = _contentView.bounds;
+    [self addChildViewController:targetViewController];
+    [_contentView addSubview:targetViewController.view];
+    
+    if ([_delegate respondsToSelector:@selector(tabBarController:didSelectViewController:)]) {
+        [_delegate tabBarController:self didSelectViewController:targetViewController];
+    }
+}
+#pragma mark - 响应 model 的地方
+#pragma mark 1 notification
+
+
+#pragma mark 2 KVO
+
+
+#pragma mark - 响应 view 的地方
+#pragma mark 1 target-action
+
+
+#pragma mark 2 delegate
+
+#pragma mark XYTabBarDelegate
+-(BOOL) tabBar:(XYTabBar *)tabBar shouldSelectIndex:(NSInteger)index
+{
+    if ([_delegate respondsToSelector:@selector(tabBarController:shouldSelectViewController:)]){
+        return [_delegate tabBarController:self shouldSelectViewController:[self.viewControllers objectAtIndex:index]];
+    }
+    return YES;
+}
+
+-(void) tabBar:(XYTabBar *)tabBar didSelectIndex:(NSInteger)index
+{
+	[self displayViewAtIndex:index];
+    
+    [self resetAnimatedView:_tabBar.animatedView index:index];
+}
+
+#pragma mark 3 dataSource
+
+
+
+@end
+
+
+@implementation UIViewController (XYTabBarController)
+
+-(XYTabBarController *) xyTabBarController
+{
+    UIViewController *vc = self.parentViewController;
+    while (vc) {
+        if ([vc isKindOfClass:[XYTabBarController class]]) {
+            return (XYTabBarController *)vc;
+        } else if (vc.parentViewController && vc.parentViewController != vc) {
+            vc = vc.parentViewController;
+        } else {
+            vc = nil;
+        }
+    }
+    return nil;
+}
+
+@end
