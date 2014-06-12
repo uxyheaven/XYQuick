1e447c4a845c97ac188a6c095413090eb29c8ba0
uxyheaven <user@UsermatoMacBook-Air.local>
2014-06-12 14:05:34 +0800

    update

--- JoinShow/JoinShow/XYExternal/Extension/IndicatorHelper.h
+++ JoinShow/JoinShow/XYExternal/Extension/IndicatorHelper.h
@@ -5,7 +5,7 @@
 //  Created by Heaven on 14-6-11.
 //  Copyright (c) 2014年 Heaven. All rights reserved.
 //
-// 指示器帮助类别
+// 指示器帮助类
 
 #import "XYPrecompile.h"
 // MBProgressHUD指示器
@@ -17,5 +17,15 @@ AS_SINGLETON(IndicatorHelper)
 
 // apple原生的UIActivityIndicatorView
 +(id) originalIndicator;
++(id) MBProgressHUD;
+
+//
+-(void) showMessage:(NSString *)message;
+
+-(void) show;
+
+
+
+-(NSTimeInterval) displayDurationForString:(NSString *)str;
 
 @end
--- JoinShow/JoinShow/XYExternal/Extension/IndicatorHelper.m
+++ JoinShow/JoinShow/XYExternal/Extension/IndicatorHelper.m
@@ -7,16 +7,73 @@
 //
 
 #import "IndicatorHelper.h"
+#import "XYExternal.h"
 
 @implementation IndicatorHelper
 
 DEF_SINGLETON(IndicatorHelper)
 
 +(id) originalIndicator{
+    /*
+    static dispatch_once_t once;
+    static UIActivityIndicatorView *activityIndicatorView;
+    dispatch_once(&once, ^ {
+        activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        activityIndicatorView.hidesWhenStopped = YES;
+    });
+    */
     UIActivityIndicatorView *view = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
     view.hidesWhenStopped = YES;
     
     return view;
 }
 
++(id) MBProgressHUD{
+    /*
+    static dispatch_once_t once;
+    static MBProgressHUD *MB_HUD;
+    dispatch_once(&once, ^ {
+        MB_HUD = [[self alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    });
+    */
+    return [[self alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+}
+
+//
+-(void) showMessage:(NSString *)message{
+    MBProgressHUD *hud = [IndicatorHelper MBProgressHUD];
+    hud.userInteractionEnabled = NO;
+    hud.mode = MBProgressHUDModeText;
+    hud.labelText = message;
+    hud.removeFromSuperViewOnHide = YES;
+    
+    [self bringViewToFront:hud];
+    
+    [hud show:YES];
+    [hud hide:YES afterDelay:[self displayDurationForString:message]];
+}
+
+
+-(void) show{
+    
+}
+
+-(NSTimeInterval) displayDurationForString:(NSString *)str{
+    return MIN((float)str.length * 0.06 + 0.3, .5);
+}
+
+-(id) indicatorView{
+    return [IndicatorHelper MBProgressHUD];
+}
+
+-(void) bringViewToFront:(UIView *)view{
+    //Getting rootViewController
+    UIViewController *topController = [UIApplication sharedApplication].keyWindow.rootViewController;
+    
+    //Getting topMost ViewController
+    while (topController.presentedViewController)
+        topController = topController.presentedViewController;
+    
+    [topController.view addSubview:view];
+}
 @end
