7bd5134df49bd597b724aaedcda319a12ce9ab82
uxyheaven <uxyheaven@163.com>
2015-05-06 17:01:40 +0800

    调整结构

---
+++ JoinShow/XYQuick/XYQuick.h
@@ -0,0 +1,11 @@
+//
+//
+//  Created by Heaven on 12-1-26.
+//  Copyright (c) 2012年 Heaven. All rights reserved.
+//
+
+#import "XYQuick_Predefine.h"   // 预编译
+#import "XYQuick_Debug.h"       // 调试
+#import "XYQuick_Event.h"       // 事件
+#import "XYQuick_Core.h"        // 基础类
+#import "XYQuick_UI.h"          // 界面
