d0e1bb2de07c77131121930d3caee88c1af0e480
XingYao <uxyheaven@163.com>
2015-10-15 21:54:26 +0800

    Release 0.8.4

--- XYQuick.podspec
+++ XYQuick.podspec
@@ -1,5 +1,5 @@
 Pod::Spec.new do |s|  
-  version            = "0.8.3"
+  version            = "0.8.4"
   s.name             = "XYQuick"  
   s.version          = version  
   s.summary          = "A quick develop utility on iOS."  
--- XYQuick/XYQuick.h
+++ XYQuick/XYQuick.h
@@ -32,7 +32,7 @@
 #define __XYQUICK_H__
 
 #undef	__XYQUICK_VERSION__
-#define	__XYQUICK_VERSION__	"0.8.3"	// 主版本号
+#define	__XYQUICK_VERSION__	"0.8.4"	// 主版本号
 
 //  在编译器的(Preprocessor Macors)里设置 (CUSTOM_XYPREDEFINE)
 //  可以定制"XYQuick_Predefine.h"里的部分编译选项
